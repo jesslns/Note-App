224c7187f69419f20f45f885593408211e61ae25
jesslns <jesslns@yahoo.com.hk>
2019-02-20 12:00:45 +0000

    rewritenote controller in module pattern

--- src/note-controller.js
+++ src/note-controller.js
@@ -1,6 +1,24 @@
-window.onload = function() {
+(function(exports) {
 
-document.getElementById("app").innerHTML = 'howdy';
-var content = document.getElementById("app").innerHTML;
-console.log(content);
+  function NoteController() {
+    document.getElementById("app").innerHTML = 'howdy';
+    var content = document.getElementById("app").innerHTML;
+    console.log(content);
+  };
+
+  //   // NoteController.prototype.renderNotes() = {
+  //   //
+  //   //   document.getElementById("app").innerHTML = 'howdy';
+  //   //   var content = document.getElementById("app").innerHTML;
+  //   //   console.log(content);
+  //   //
+  //   // };
+
+  exports.NoteController = NoteController
+
+})(this);
+
+
+window.onload = function() {
+var noteController = new NoteController();
 };
